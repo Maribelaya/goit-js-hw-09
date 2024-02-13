ebf8cc10470aadba72e2ee8fc5e46a130c6b622c
Maribelaya <shandermarina7@gmail.com>
2024-02-13 17:25:40 +0100

    hw-9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,39 +1,60 @@
-let form = document.querySelector('form');
-let localStorageKey = "feedback-form-state";
 
-const formData = {email: "", message: ""};
+let form = document.querySelector('form');
+//Ключ до сховища
+let localStorageKey = "feedback-form-state";
+//Об"єкт, що містить дані форми
+let formData = {email: "", message: ""};
+
+//Отримання даних зі сховища за ключем (при запускі)
 let localStorageItem = localStorage.getItem(localStorageKey);
 
+//Якщо дані зі сховища отримано (тобто вони були збережені у сховищі раніше)
 if(localStorageItem){
+  //Формування об"єкту, за даними сховища
   formData = JSON.parse(localStorageItem);
-  form.email.value = formData.email;
-  form.message.value = formData.message;
+  //Заповнення елементів форми
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
 }
 
+//Подія - введення даних
 form.addEventListener('input', event => {
-    formData.email = event.target.value.trim();
-    formData.message = event.target.value.trim();
+  //Делегуванням подій визначаємо у якому саме елементі форми відбулась подія (що вводили, адресу чи текст)
+  
+  if(event.target.name == "email"){ //Вводили адресу 
+    formData.email = event.target.value.trim(); //Метод trim(), щоб прибрати зайві пробіли в значеннях
+  }
 
+  if(event.target.name == "message"){ //Вводили текст
+    formData.message = event.target.value.trim(); //Метод trim, щоб прибрати зайві пробіли в значеннях
+  }
+
+  //Збереження даних у сховище
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
+//Подія - відправка форми
 form.addEventListener('submit', event => {
+  //Відміна стандартної події
   event.preventDefault();
 
-  if(form.email.value && form.message.value)
+  //Перевірка, чи заповнені поля форми
+  if(form.elements.email.value &&  form.elements.message.value)
   {
-    form.email.value = "";
-    form.message.value = "";
+    //Очищення полів на формі
+    form.elements.email.value = "";
+    form.elements.message.value = "";
+    
+    //Запис у консоль об"єкту, що містить дані форми
     console.log(formData);
 
+    //Очищення сховища
     localStorage.removeItem(localStorageKey);
   }
   else
   {
-    if (email === '' || message === '') {
-      alert('Будь ласка, заповніть всі поля.');
-      return;
-  }
+    //Не заповнені усі поля форми
+    alert('Будь ласка, заповніть всі поля.');
   }
 });
 
